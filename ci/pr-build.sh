955891f276aef7a3654d8a8f5c2a4a88e2391ee7
Daniel Flook <daniel@flook.org>
2024-06-10 15:54:08 +0100

    make it more like the real repo

--- ci/pr-build.sh
+++ ci/pr-build.sh
@@ -2,4 +2,4 @@
 
 echo "Building for the PR branch"
 
-aws cloudformation create-change-set --stack-name Roles --change-set-name $(echo $CODEBUILD_WEBHOOK_TRIGGER | tr '/' '-')-$CODEBUILD_RESOLVED_SOURCE_VERSION --description "" --template-body file://output/stack-definitions/flooktech.yaml --capabilities CAPABILITY_NAMED_IAM
+aws cloudformation create-change-set --stack-name Roles --change-set-name $(echo $CODEBUILD_WEBHOOK_TRIGGER | tr '/' '-')-$CODEBUILD_RESOLVED_SOURCE_VERSION --template-body file://output/stack-definitions/flooktech.yaml --capabilities CAPABILITY_NAMED_IAM
